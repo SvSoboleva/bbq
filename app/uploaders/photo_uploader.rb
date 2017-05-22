07713d987c606cd4502869a4a5684bc745f1d19c
Lana <sv.soboleva@list.ru>
2017-05-22 20:05:59 +0300

    checking blank photo

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -39,6 +39,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
+
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
